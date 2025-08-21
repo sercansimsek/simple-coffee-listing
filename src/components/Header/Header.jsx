35064dcd5bf345c60b2f9d4d7606d6f1f3233467
Sercan <sercansimsek44@gmail.com>
2025-08-21 15:38:49 +0200

    styles for mobile screens are added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,27 @@
-import { CoffeeList } from '../CoffeeList'
+import { CoffeeList } from "../CoffeeList";
+import "./Header.css";
 
 export const Header = () => {
 	return (
 		<div className="header">
-			<h1 className="header-title">Our Collection</h1>
-			<p className="header-description">
-				Introducing our Coffee Collection, a selection of unique coffees from
-				different roast types and origins, expertly roasted in small batches and
-				shipped fresh weekly.
-			</p>
+      <img src="/src/assets/images/vector.svg" alt="logo" className="header-hero" />
+			<div className="header-text">
+				<h1 className="header-title">Our Collection</h1>
+				<p className="header-description">
+					Introducing our Coffee Collection, a selection of unique coffees from
+					different roast types and origins, expertly roasted in small batches
+					and shipped fresh weekly.
+				</p>
+			</div>
 
-      <CoffeeList />
+			<div className="header-buttons">
+				<button className="btn-all">All Products</button>
+				<button className="btn-available">Available Now</button>
+			</div>
+
+			<div>
+				<CoffeeList />
+			</div>
 		</div>
 	);
 };
